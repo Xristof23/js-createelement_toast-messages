75168700d11b355e0971666a724012cd2b7ff220
Christof Röhl <userpattern@gmail.com>
2024-01-12 10:59:41 +0100

    solved

--- js/index.js
+++ js/index.js
@@ -6,8 +6,17 @@ const clearButton = document.querySelector('[data-js="clear-button"]');
 
 addButton.addEventListener("click", () => {
   // Exercise: Append a new entry to the toast messages container
+  const toastMessage = document.createElement("li");
+  toastMessage.classList.add("toast-container__message");
+  document.body.append(toastContainer);
+  toastContainer.append(toastMessage);
+  toastMessage.textContent = "Hi there!";
+  console.log("Button works");
 });
 
 clearButton.addEventListener("click", () => {
   // Exercise: Clear the stack of toast messages
+toastContainer.innerHTML = "";
 });
+/* document.body.append(article); // placing the created article at the end of the body
+article.append(button); // placing the created button into the article */
